b657454c69a466e2dca411c0c7b653a9937346c3
UnoSD <unosd.dev@gmail.com>
2017-08-02 14:01:33 +0100

    Change config to point to local hdfs for jars

--- SparkSharp/CosmosExampleSessionConfiguration.cs
+++ SparkSharp/CosmosExampleSessionConfiguration.cs
@@ -7,14 +7,14 @@
             {
                 Kind = LivySessionKind.Spark,
                 Name = "SparkSharp testing",
-                ExecutorMemory = "11G",
-                ExecutorCores = 15,
-                NumExecutors = 4,
-                DriverCores = 4,
+                ExecutorMemory = "1G",
+                ExecutorCores = 2,
+                NumExecutors = 2,
+                DriverCores = 2,
                 Jars =
                 {
-                    "wasb:///example/jars/azure-cosmosdb-spark-0.0.3-SNAPSHOT.jar",
-                    "wasb:///example/jars/azure-documentdb-1.10.0.jar"
+                    "hdfs://master.hadoop.lan:9000/azure-cosmosdb-spark-0.0.3-SNAPSHOT.jar",
+                    "hdfs://master.hadoop.lan:9000/azure-documentdb-1.12.0.jar"
                 },
                 Conf =
                 {
